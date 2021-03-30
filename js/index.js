f5b9374e2e104b7f532e32a32d421df67161959f
Ankit Aggarwal <ankitaggarwal.cse18@chitkarauniversity.edu.in>
2021-03-30 14:01:16 +0530

    project finished

--- js/index.js
+++ js/index.js
@@ -1,16 +1,18 @@
 var signIn= document.getElementById('signIn');
 var signIn= document.getElementById('signUp');
-var signInModel=document.getElementById('model2');
-var signUpModel=document.getElementById('model1');
+var signInModel=document.getElementById('welcomeBackModal');
+var signUpModel=document.getElementById('getStartedModal');
 var mainModel=document.getElementById('myModal');
 var closeBtn= document.getElementById('modelCloseBtn');
-var postsModel=document.getElementById('model3');
+var postsModel=document.getElementById('penYourPostModal');
+
 function signInfun(){
   signInModel.style.display="block";
   mainModel.style.display="block";
   signUpModel.style.display="none";
   postsModel.style.display="none";
 }
+
 function signUpfun(){
     signUpModel.style.display="block";
     mainModel.style.display="block";
@@ -21,6 +23,7 @@ function signUpfun(){
 function closeModelfun(){
     mainModel.style.display="none";
 }
+
 function postfun(){
   postsModel.style.display="block";
   mainModel.style.display="block";
